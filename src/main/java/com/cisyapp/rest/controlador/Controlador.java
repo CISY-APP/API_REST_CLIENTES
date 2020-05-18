90e6a5183c129099e7ee0529b1ab0cae6828f36e
Rafa <rafael.garcia.fernandez.87@gmail.com>
2020-05-19 00:54:32 +0200

    Comentado el codigo de usuario y control para clave

--- src/main/java/com/cisyapp/rest/controlador/Controlador.java
+++ src/main/java/com/cisyapp/rest/controlador/Controlador.java
@@ -49,10 +49,32 @@ public class Controlador {
 	
 	
 	//REGISTRO USUARIOS 
-	//(Añadir metodo para comprobar la clave)
-	@RequestMapping(value = "/registrarUsuario", method = RequestMethod.POST)
-	public ResponseEntity<Usuario> registraUsuario(@Valid @RequestBody Usuario usuario) {
-		return new ResponseEntity<Usuario>(UsuarioServicio.registraUsuario(usuario), HttpStatus.OK);
+	@RequestMapping(value = "/registrarUsuario", method = RequestMethod.POST) //Definimos la URI a la que nos deben enviar las peticiones y el metodo http
+	public ResponseEntity<Usuario> registraUsuario(@Valid @RequestBody Usuario usuario) { //Metodo que devuelve una entidad Usuario y recibe mediante el
+																							//body un objeto de tipo usuario
+		String auxClave=usuario.getClave(); 								//Comprobación de que la clave de usuario esta formada correctamente
+		boolean letraMayuscula=false, letraMinuscula=false, numero=false;	//debe tener 1 mayusucula, 1 minuscula y un numero
+		
+			for(int i=0; i<auxClave.length() || letraMayuscula==false || letraMinuscula==false || numero==false;i++) {//Recorremos el String para comprobar
+																														//que contiene el formato correcto
+				if(auxClave.charAt(i)>'A' || auxClave.charAt(i)<'Z') {	//Si encontramos una mayuscula actualizamos el boolean a true
+					letraMayuscula=true;
+				}
+				if(auxClave.charAt(i)>'a' || auxClave.charAt(i)<'z') {	//Si encontramos una minuscula actualizamos el boolean a true
+					letraMinuscula=true;
+				}
+				
+				if(auxClave.charAt(i)>'0' || auxClave.charAt(i)<'9') {	//Si encontramos un numero actualizamos el boolean a true
+					numero=true;
+				}
+			}
+			
+			if(letraMayuscula==true && letraMinuscula==true && numero==true) {	//Si la clave tiene el formato correcto registramos el usuario
+				return new ResponseEntity<Usuario>(UsuarioServicio.registraUsuario(usuario), HttpStatus.OK);
+			}else {
+				return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);	//Si la clave no tiene el formato correcto devolvemos un codigo de error
+			}
+		
 	}
 	
 	
@@ -76,51 +98,54 @@ public class Controlador {
 	
 	
 	//MOSTRAR USUARIO POR ID
-	@RequestMapping(value = "/consultarUsuarioPorId/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Usuario> consultaUsuarioPorId(@PathVariable("id") Integer id) {
-	 	Optional<Usuario>uOpt=UsuarioServicio.consultaUsuarioPorId(id);
+	@RequestMapping(value = "/consultarUsuarioPorId/{id}", method = RequestMethod.GET)//Definimos la URI a la que nos deben enviar las peticiones y 
+																						//el metodo http
+	public ResponseEntity<Usuario> consultaUsuarioPorId(@PathVariable("id") Integer id) {//Metodo que devuelve una entidad Usuario y recibe mediante el
+																						//body un objeto de tipo usuario
+	 	Optional<Usuario>uOpt=UsuarioServicio.consultaUsuarioPorId(id);	//Llamamos al metodo del servicio consultaUsuarioPorId que nos devuelve un usuario
 	 		if(uOpt.isPresent()) {
-	 			Usuario u=uOpt.get();
+	 			Usuario u=uOpt.get();									//Si el usuario existe, lo devolvemos
 	 			return new ResponseEntity<Usuario>(u, HttpStatus.OK);
-	 		}else {
+	 		}else {														//Si el usuario NO existe devolvemos un código de error
 	 			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 	 		}	
 		}
 	
 	
 	//ACTUALIZAR USUARIO
-	 @RequestMapping(value = "/actualizarUsuario", method = RequestMethod.PUT)//Asociamos la petición recibida la metodo de respuesta
-	   	public ResponseEntity<Usuario> actualizaUsuarioPorID(@RequestBody Map<String, String> param) {
+	 @RequestMapping(value = "/actualizarUsuario", method = RequestMethod.PUT)//Definimos la URI a la que nos deben enviar las peticiones y el metodo http
+	   	public ResponseEntity<Usuario> actualizaUsuarioPorID(@RequestBody Map<String, String> param) {//Metodo que actualiza los datos del usuario y 
+		 																								//devuelve el usuario actualizado
 		 
-		 	Integer auxIdUsuario=Integer.parseInt(param.get("idUsuario"));
+		 	Integer auxIdUsuario=Integer.parseInt(param.get("idUsuario"));	//Comprobamos que el usuario existe
 		 	Optional<Usuario> uOpt=UsuarioServicio.consultaUsuarioPorId(auxIdUsuario);
 	    	
-		 	if(uOpt.isPresent()) {
+		 	if(uOpt.isPresent()) {											//Si el usuario existe creamos una entidad usuario con los datos de la BD
 		 		Usuario u=uOpt.get();
 		 		
-		 		if(!param.get("telefono").equals("")) {
+		 		if(!param.get("telefono").equals("")) {						//Si el atributo telefono que nos han pasado tiene contenido lo verificamos
 		 			try {
-		 				Integer auxTelefono=Integer.parseInt(param.get("telefono"));
-		 				if(param.get("telefono").length()==9) {
-		 					u.setTelefono(auxTelefono);
+		 				Integer auxTelefono=Integer.parseInt(param.get("telefono"));	//Verificamos que el telefono sea de tipo Integer
+		 				if(param.get("telefono").length()==9) {							//si es de tipo Integer, comprobamos que sean 9 digitos
+		 					u.setTelefono(auxTelefono);									//si todo es correcto establecemos el nuevo telefono
 		 				}else {
-		 					return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		 					return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);	//Si no tiene 9 digitos lanzamos un codigo de error
 		 				}
-		 			}catch(NumberFormatException n){
-		 				return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		 			}catch(NumberFormatException n){	
+		 				return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);		//Si no es de tipo Integer lanzamos un codigo de error
 		 			}
 		 		}
 		 		
-		 		if(!param.get("fechaNac").equals("")) {
-		 			u.setFechanacimiento(Date.valueOf(param.get("fechaNac")));
+		 		if(!param.get("fechaNac").equals("")) {									//Comprobamos que el atributo fecha Nacimiento tiene contenido
+		 			u.setFechanacimiento(Date.valueOf(param.get("fechaNac")));			//si tiene contenido establecemos la nueva fecha
 		 		}
-		 		if(!param.get("descripcion").equals("")) {
-		 			u.setDescripcion(param.get("descripcion"));
+		 		if(!param.get("descripcion").equals("")) {								//Comprobamos que el altributo descripción tiene contenido
+		 			u.setDescripcion(param.get("descripcion"));							//si tiene contenido establecemos la nueva descripción
 		 		}
 		 		
-		 		return ResponseEntity.ok(UsuarioServicio.actualizaUsuario(u));
+		 		return ResponseEntity.ok(UsuarioServicio.actualizaUsuario(u));			//Llamamos al metodo encargado de actualizar los datos
 		 	}else {
-		 		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		 		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);				//Si el usuario no existe devolvemos un código de error
 		 	}
 	    
 	    		
@@ -128,25 +153,46 @@ public class Controlador {
 	 
 	 
 	 //ACTUALIZAR CLAVE DE USUARIO POR ID USUARIO
-	 @RequestMapping(value = "/actualizarClaveUsuario", method = RequestMethod.PUT)//Asociamos la petición recibida la metodo de respuesta
-	   	public ResponseEntity<Usuario> actualizaClaveUsuario(@RequestBody Map<String, String> param) {
-		 
-		 	Integer auxIdUsuario=Integer.parseInt(param.get("idUsuario"));
-		 	Optional<Usuario> uOpt=UsuarioServicio.consultaUsuarioPorId(auxIdUsuario);
+	 @RequestMapping(value = "/actualizarClaveUsuario", method = RequestMethod.PUT) //Definimos la URI a la que nos deben enviar las peticiones 
+	 																				//y el metodo http
+	   	public ResponseEntity<Usuario> actualizaClaveUsuario(@RequestBody Map<String, String> param) { //Metodo encargado de actualizar la clave
+		 																								//del usuario
+		 	Integer auxIdUsuario=Integer.parseInt(param.get("idUsuario"));								//Consultamos si el usuario existe mediante el
+		 	Optional<Usuario> uOpt=UsuarioServicio.consultaUsuarioPorId(auxIdUsuario);					//id de usuario
 	    	
-		 	if(uOpt.isPresent()) {
-		 		Usuario u=uOpt.get();
-		 		
-		 		String auxClaveActual=param.get("claveActual");
-		 		if(u.getClave().equals(auxClaveActual)) {
-		 			u.setClave(param.get("nuevaClave"));
-		 			return ResponseEntity.ok(UsuarioServicio.actualizaUsuario(u));
+		 	if(uOpt.isPresent()) {														//Si el usuario existe
+		 		Usuario u=uOpt.get();													//Establecemos un usuario con los datos de la BD
+		 		String auxClaveActual=param.get("claveActual");							//Comprobamos que la clave actual es correcta
+		 		if(u.getClave().equals(auxClaveActual)) {								//Si la clave actual es correcta, comprobamos que la nueva
+		 																				//clave tiene un formato correcto
+		 			
+		 			boolean letraMayuscula=false, letraMinuscula=false, numero=false;
+		 			String auxNuevaClave=param.get("nuevaClave");						//Comprobamos que la clave tiene un formato correcto recorriendola
+		 			for(int i=0; i<auxNuevaClave.length() || letraMayuscula==false || letraMinuscula==false || numero==false;i++) {
+		 				if(auxNuevaClave.charAt(i)>'A' || auxNuevaClave.charAt(i)<'Z') { //Comprobamos que existe una letra mayuscula
+		 					letraMayuscula=true;
+		 				}
+		 				if(auxNuevaClave.charAt(i)>'a' || auxNuevaClave.charAt(i)<'z') { //Comprobamos que existe una letra minuscula
+		 					letraMinuscula=true;
+		 				}
+		 				
+		 				if(auxNuevaClave.charAt(i)>'0' || auxNuevaClave.charAt(i)<'9') { //Comprobamos que existe un numero
+		 					numero=true;
+		 				}
+		 			}
+		 			if(letraMayuscula==true && letraMinuscula==true && numero==true) {	//Si todas las comprobaciones son correctas
+		 				u.setClave(param.get("nuevaClave"));							//Asignamos la nueva clave
+			 			return ResponseEntity.ok(UsuarioServicio.actualizaUsuario(u));	//Llamamos al metodo encargado de actualizar los datos
+		 			}else {
+		 				return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);		//Si falla alguna comprobación devolvemos un codigo de error
+		 			}
+		 			
 		 		}else {
-		 			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		 			return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);			//Si la clave introducida es incorrecta devolvemos un error
 		 		}
 		 		
 		 	}else {
-		 		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		 		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);				//Si el usuario no existe devolvemos un codigo de error
 		 	}
 	    
 	    		
@@ -154,14 +200,16 @@ public class Controlador {
 	 
 	 
 	 //ELIMINAR USUARIO POR ID
-	 @RequestMapping(value = "/eliminarUsuario/{id}", method = RequestMethod.DELETE)//Asociamos la petición recibida la metodo de respuesta
-		public ResponseEntity<Usuario> eliminarUsuarioPorId(@PathVariable("id") Integer id) {
-		    Optional<Usuario> uOpt=UsuarioServicio.consultaUsuarioPorId(id);
+	 @RequestMapping(value = "/eliminarUsuario/{id}", method = RequestMethod.DELETE)//Definimos la URI a la que nos deben enviar las peticiones y el
+	 																				//el metodo http
+		public ResponseEntity<Usuario> eliminarUsuarioPorId(@PathVariable("id") Integer id) { //Metodo que elimina a un usuario por id
+		    Optional<Usuario> uOpt=UsuarioServicio.consultaUsuarioPorId(id);				//Comprobams si el usuario existe
 		    if (uOpt.isPresent()) {
-		    	Usuario u= uOpt.get();
-		    	return new ResponseEntity<>(UsuarioServicio.eliminaUsuario(u),HttpStatus.OK);//Si existe eliminamos el vehiculo solicitado
+		    	Usuario u= uOpt.get();														//Si existe establecemos a un objeto usuario los valores 
+		    																				//asociados al id recibido
+		    	return new ResponseEntity<>(UsuarioServicio.eliminaUsuario(u),HttpStatus.OK);//Si existe eliminamos el usuario solicitado
 		    }else {	
-		    	return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+		    	return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);						//Si el usuario no existe devolvemos un código de error
 		    }
 		   
 		}
--- src/main/java/com/cisyapp/rest/modelo/Usuario.java
+++ src/main/java/com/cisyapp/rest/modelo/Usuario.java
@@ -18,6 +18,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -34,18 +35,22 @@ import javax.validation.constraints.Size;
 public class Usuario implements java.io.Serializable {
 
 	private Integer idusuario;
+	@NotNull	
 	@Size(min=1, max=20)
 	@Pattern(regexp = "[a-z A-Z]+", message="El nombre solo puede estar formado por letras")
 	private String nombre;
+	@NotNull
 	@Size(min=1, max=20)
 	@Pattern(regexp = "[a-z A-Z]+", message="Los apellidos solo pueden estar formados por letras")
 	private String apellidos;
 	private Boolean espasajero;
 	private Boolean esconductor;
 	private Integer telefono;
+	@NotNull
 	@Size(max=30)
 	@Email(message="Introduzca un formato de correo válido")
 	private String email;
+	@NotNull
 	@Size(min=6, max=30)
 	@Pattern(regexp = "[a-zA-Z0-9]+", message="la clave no admite caracteres especiales")
 	private String clave;
